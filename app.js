d18ea853ad0da1b7aa9dd7668a0243d9b1c5c42f
I-A11 <d.islam1983@gmail.com>
2021-08-16 17:58:38 +1000

    adding top-link arrow

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const toggleBtn = document.querySelector(".nav-toggle");
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
 });
+
+// top-link
+const topLink = document.querySelector(".top-link");
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
